26cbfef4be4cf0973eaf479fbf990f18ca300513
Melodi Balossino <melodibalossino@gmail.com>
2024-02-25 12:31:41 -0300

    reverseString

--- docs/index.js
+++ docs/index.js
@@ -4,8 +4,14 @@
 */
 
 function reverseString(str) {
-  // Tu solución acá  
+  // Tu solución acá 
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 }
+reverseString("Melodi"); 
 
 /*
   Ejercicio 2: Check for Palindrome
