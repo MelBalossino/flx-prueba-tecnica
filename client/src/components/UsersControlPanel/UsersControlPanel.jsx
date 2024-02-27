f731e149b5fb40ac8db1fc6c6ee0f890fe94bad9
Melodi Balossino <melodibalossino@gmail.com>
2024-02-27 17:13:40 -0300

    Feat/User-control-panel

---
+++ client/src/components/UsersControlPanel/UsersControlPanel.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import SearchUser from '../SearchUser/SearchUser';
+import FilterUserByStatus from '../FilterUsersByStatus/FilterUserByStatus';
+import AddUserButton from '../AddUserButton/AddUserButton';
+import styles from './UsersControlPanel.module.css';
+import { Typography } from 'antd';
+
+const { Title, Text } = Typography;
+
+const UsersControlPanel = () => {
+    return (
+        <>
+            <div className={styles.title}>
+                <Title level={1}>
+                    <Text type='secondary'>Usuarios / </Text>
+                    <Text strong>Listado de usuarios</Text>
+                </Title>
+            </div>
+            <div className={styles.toolbar}>
+                <div className={styles.leftPart}>
+                    <div className={styles.searchPart}>
+                        <SearchUser />
+                    </div>
+                    <div className={styles.statusFilterPart}>
+                        <FilterUserByStatus />
+                    </div>
+                </div>
+                <div className={styles.addButtonPart}>
+                    <AddUserButton />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UsersControlPanel;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import styles from "./Home.module.css";
 import Header from "../../components/Header/Header";
 import UserTable from "../../components/UserTable/UserTable";
-import AddUserButton from "../../components/AddUserButton/AddUserButton";
-import SearchUser from "../../components/SearchUser/SearchUser";
-import FilterUserByStatus from "../../components/FilterUsersByStatus/FilterUserByStatus";
+import UsersControlPanel from "../../components/UsersControlPanel/UsersControlPanel";
 
 const Home = () => {
     return (
         <>
             <Header />
-            <SearchUser />
-            <FilterUserByStatus />
-            <AddUserButton classname={styles.addButton} />
+            <UsersControlPanel />
             <UserTable />
         </>
     );
